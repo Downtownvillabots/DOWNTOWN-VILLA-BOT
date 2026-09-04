dfb8ae261b46ec20343392863505d79c5b93e540
DOWNTOWNVILLA <downtownvillaofficial@gmail.com>
2026-09-04 15:13:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,11 @@ from Script import script
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_command(client: Client, message: Message):
-    await message.reply_text(
-        script.START_TEXT.format(message.from_user.first_name, BOT_NAME),
-        parse_mode="HTML"
-    )
+    try:
+        await message.reply_text(
+            script.START_TEXT.format(message.from_user.first_name, BOT_NAME),
+            parse_mode="HTML"
+        )
+        print(f"User {message.from_user.id} started")
+    except Exception as e:
+        print(f"ERROR in start: {e}")
