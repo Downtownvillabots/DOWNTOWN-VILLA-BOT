f4cffc7a6815fa8d0b51288fe75b3d734b86e5ad
DOWNTOWNVILLA <downtownvillaofficial@gmail.com>
2026-09-04 14:24:58 +0530

    Add basic /start command implementation

---
+++ plugins/start.py
@@ -0,0 +1,12 @@
+# plugins/start.py – Basic /start command
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import BOT_NAME
+from Script import script
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start_command(client: Client, message: Message):
+    await message.reply_text(
+        script.START_TEXT.format(message.from_user.first_name, BOT_NAME),
+        parse_mode="HTML"
+    )
