ef9e5b75d601b81270fd0cb717d70ae7999b0686
DOWNTOWNVILLA <downtownvillaofficial@gmail.com>
2026-09-12 11:01:10 +0530

    Delete plugins/start.py

--- plugins/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start_command(client: Client, message: Message):
-    await message.reply_text(
-        "✅ **DOWNTOWN VILLA BOT** is running.\n\n"
-        "Features will be added soon."
-    )
