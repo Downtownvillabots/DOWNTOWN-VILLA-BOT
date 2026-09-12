e7001246271e398949349e53f55f0f9c30abc19f
DOWNTOWNVILLA <downtownvillaofficial@gmail.com>
2026-09-12 11:13:26 +0530

    Change database reference from core to system

--- database/core/monitoring_state.py
+++ database/core/monitoring_state.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Any
 from database import db_registry
 
 class MonitoringStateRepository:
@@ -7,7 +7,7 @@ class MonitoringStateRepository:
 
     def _get_collection(self):
         if self._collection is None:
-            db = db_registry.get_core_db()
+            db = db_registry.get_system_db()
             self._collection = db["monitoring_state"]
         return self._collection
 
